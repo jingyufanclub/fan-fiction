680ca4292edf0a2c8645c8cc046035ac57c24f0d
Jing <jingyufanclub@gmail.com>
2021-06-08 01:28:53 -0400

    image

--- good-morning-world/scripts/script.js
+++ good-morning-world/scripts/script.js
@@ -2,7 +2,7 @@ let number = 1;
 const image = document.getElementById("photobooth")
 
 function tomorrow() {
-  if (number === 33) {
+  if (number === 34) {
     number = 0
   }
   number ++
@@ -11,7 +11,7 @@ function tomorrow() {
 
 function yesterday() {
   if (number === 1) {
-    number = 34
+    number = 35
   }
   number--
   image.src = `images/${number}.jpg`
