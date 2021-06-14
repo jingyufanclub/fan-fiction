6f0efbe9afe5e8a4832b07d2c72cf22656f4e299
Jing <jingyufanclub@gmail.com>
2021-06-13 20:41:11 -0400

    footer and styles

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+console.log("%cYou just haven't earned it yet, baby.", "color:cornflowerblue; font-family: serif; font-size:20px; font-style:italic")
+
+document.getElementById("year").innerHTML = new Date().getFullYear()
