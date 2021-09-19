d517c0fe51223e5b70bd33ed84cf7b0f0e823b9e
Jing <jingyufanclub@gmail.com>
2021-09-19 19:14:11 -0400

    new pages

--- good-morning-world/scripts/script.js
+++ good-morning-world/scripts/script.js
@@ -1,14 +1,16 @@
-function dayOfYear(date) {
-  return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
-}
+window.onload = function () {
+  function dayOfYear(date) {
+    return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+  }
 
-const image = document.getElementById("photobooth")
+  const image = document.getElementById("photobooth")
 
-const index = dayOfYear(new Date()) % 34;
-image.src = `images/${index}.jpg`
+  const index = dayOfYear(new Date()) % 34;
+  image.src = `images/${index}.jpg`
 
-const end = document.getElementById("end")
-if (index === 0) {
-  end.style.fontFamily = 'Parisienne, cursive';
-  end.innerHTML = "~ The End ~"
+  const end = document.getElementById("end")
+  if (index === 0) {
+    end.style.fontFamily = 'Parisienne, cursive';
+    end.innerHTML = "~ The End ~"
+  }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
-console.log("%cYou just haven't earned it yet, baby.", "color:cornflowerblue; font-family: serif; font-size:20px; font-style:italic")
+window.onload = function () {
 
-document.getElementById("year").innerHTML = new Date().getFullYear()
+  console.log("%cYou just haven't earned it yet, baby.", "color:cornflowerblue; font-family: serif; font-size:20px; font-style:italic")
+
+  document.getElementById("year").innerHTML = new Date().getFullYear()
+}
