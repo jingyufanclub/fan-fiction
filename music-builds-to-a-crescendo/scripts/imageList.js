3f0623c5e34cb364cf958ded087cd5e375fdaf53
Jing <jingyufanclub@gmail.com>
2021-12-31 23:54:10 -0500

    use header video

---
+++ music-builds-to-a-crescendo/scripts/imageList.js
@@ -0,0 +1,26 @@
+const imageList = [
+  {
+    src: "mm_dressing.jpg",
+    alt: "Mark Morris dressing room"
+  },
+  {
+    src: "pink_fifth.jpg",
+    alt: "Wearing an all pink outfit, standing in fifth position"
+  },
+  {
+    src: "pdd_1.jpg",
+    alt: "Sheet music for Tchaikovsky Pas de Deux, page 1"
+  },
+  {
+    src: "pink_saute.jpg",
+    alt: "In the same outfit performing a small sauté"
+  },
+  {
+    src: "pdd_2.jpg",
+    alt: "Sheet music for Tchaikovsky Pas de Deux, page 2"
+  },
+  {
+    src: "pink_parallel.jpg",
+    alt: "Landed the jump, standing in parralel"
+  },
+];
--- music-builds-to-a-crescendo/scripts/script.js
+++ music-builds-to-a-crescendo/scripts/script.js
@@ -1,37 +1,45 @@
-let images;
-
-window.addEventListener("load", function (event) {
-  images = document.querySelectorAll(".lazy-image");
+window.onload = function () {
+  const gallery = document.getElementById("gallery");
+  imageList.forEach(
+    image => {
+      const img = document.createElement("img");
+      img.className = "lazy-image";
+      img.alt = image.alt
+      img.setAttribute("data-src", `images/${image.src}`);
+      gallery.appendChild(img);
+    }
+  );
+  const images = document.querySelectorAll(".lazy-image");
   createObserver();
-}, false);
 
-function createObserver() {
-  let observer;
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.4
-  };
+  function createObserver() {
+    let observer;
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.4
+    };
 
-  if ("IntersectionObserver" in window) {
-    observer = new IntersectionObserver(handleIntersect, options);
-    images.forEach(image => {
-      observer.observe(image);
-    });
-  } else {
-    images.forEach(image => {
-      image.src = image.dataset.src;
+    if ("IntersectionObserver" in window) {
+      observer = new IntersectionObserver(handleIntersect, options);
+      images.forEach(image => {
+        observer.observe(image);
+      });
+    } else {
+      images.forEach(image => {
+        image.src = image.dataset.src;
+      });
+    }
+  }
+
+  function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let img = entry.target;
+        img.src = img.dataset.src;
+        img.classList.add("fade-in");
+        observer.unobserve(img);
+      }
     });
   }
-}
-
-function handleIntersect(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      let img = entry.target;
-      img.src = img.dataset.src;
-      img.classList.add("fade-in");
-      observer.unobserve(img);
-    }
-  });
 }
